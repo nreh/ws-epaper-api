282896d8ee7c9716cb7431478b5ea3010b3c21ae
Naqeeb Rehman <naq.rehman@gmail.com>
2024-11-03 17:17:16 -0500

    Added Physical EPD class

--- external/waveshare/EPD_Common.h
+++ external/waveshare/EPD_Common.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <bufferutils.h>
+#include <memory>
 #include <stdexcept>
 #include <string>
+#include <wsepaperapi.h>
+
+#include "DEV_Config.h"
 
 namespace epaperapi {
 
@@ -16,6 +21,30 @@ class UnsupportedRefreshMode : public std::runtime_error {
   public:
     explicit UnsupportedRefreshMode(const RefreshMode refreshMode, const std::string deviceName)
         : std::runtime_error("The device '" + deviceName + "' deos not support this refresh mode") {}
+};
+
+/// @brief All physical EPD devices inherit from this abstract class. Common logic between displays is implemented here.
+class PhysicalEPD : public AbstractDrawTarget {
+  protected:
+    bool Open = false;
+
+  public:
+    /// @brief Whether or not the SPI connection / pins are set up. Calling `Exit()` will result in this returning false.
+    /// @return
+    bool IsOpen() { return Open; }
+
+    /// @brief Initialize pins and SPI protocol. You shouldn't have to run this manually as this is run automatically when
+    /// the class is initialized.
+    void InitializeSPI() {
+        DEV_Module_Init();
+        Open = true;
+    }
+
+    /// @brief Shuts off power to the device, ends SPI protocol, and deallocates memory
+    void Exit() {
+        DEV_Module_Exit();
+        Open = false;
+    }
 };
 
 } // namespace devices
