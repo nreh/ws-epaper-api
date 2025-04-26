218fb105810a7039e88f814d77ca6077246f1137
Naqeeb Rehman <naq.rehman@gmail.com>
2025-04-26 17:53:45 -0400

    Fixed EPD_13in3b

--- external/waveshare/DrawTargets/EPD_13in3b.h
+++ external/waveshare/DrawTargets/EPD_13in3b.h
@@ -50,7 +50,7 @@
  *   "BitmapFunction": "GUI_ReadBmp"
  * }
  */
- 
+
 #pragma once
 
 #include "../EPD_Common.h"
@@ -76,57 +76,42 @@ const int DEVICE_WIDTH = 960;
 /// @brief Height of device in pixels
 const int DEVICE_HEIGHT = 680;
 
-enum RefreshMode { Display = 0, Base = 1, Base_White = 2 };
+enum RefreshMode { Display = 0, Base = 1 };
 
 class EPD_13in3b_DrawTarget : public RedBlack1BitEPD {
   public:
     std::string GetDeviceName() const override { return "13.3inch e-paper (B) V1.0"; }
     int GetWidth() const override { return DEVICE_WIDTH; }
     int GetHeight() const override { return DEVICE_HEIGHT; }
-    
+
     // The following functions were not created:
 
-    //  ! EPD_13IN3B_Display_Partial(const UBYTE *Image, UWORD Xstart, UWORD Ystart, UWORD Xend, UWORD Yend) was skipped because I'm not sure what arguments to pass in!
+    //  ! EPD_13IN3B_Display_Partial(const UBYTE *Image, UWORD Xstart, UWORD Ystart, UWORD Xend, UWORD Yend) was skipped
+    //  because I'm not sure what arguments to pass in!
 
     /// @brief Initialize the display
-    void Init() {
-        controller::EPD_13IN3B_Init();
-    }
+    void Init() { controller::EPD_13IN3B_Init(); }
 
     /// @brief Clear the display
-    void Clear() {
-        controller::EPD_13IN3B_Clear();
-    }
+    void Clear() { controller::EPD_13IN3B_Clear(); }
 
     /// @brief Clear the display
-    void Clear_Red() {
-        controller::EPD_13IN3B_Clear_Red();
-    }
+    void Clear_Red() { controller::EPD_13IN3B_Clear_Red(); }
 
     /// @brief Clear the display
-    void Clear_Black() {
-        controller::EPD_13IN3B_Clear_Black();
-    }
+    void Clear_Black() { controller::EPD_13IN3B_Clear_Black(); }
 
     /// @brief Put the display to sleep
-    void Sleep() {
-        controller::EPD_13IN3B_Sleep();
-    }
+    void Sleep() { controller::EPD_13IN3B_Sleep(); }
 
     /// @brief Display pixels in buffers to display
-    void Display() {
-        controller::EPD_13IN3B_Display(packedBitsBlack, packedBitsRed);
-    }
+    void Display() { controller::EPD_13IN3B_Display(packedBitsBlack, packedBitsRed); }
 
     /// @brief Display pixels in buffers to display
-    void Display_Base() {
-        controller::EPD_13IN3B_Display_Base(packedBitsBlack, packedBitsRed);
-    }
+    void Display_Base() { controller::EPD_13IN3B_Display_Base(packedBitsBlack, packedBitsRed); }
 
     /// @brief Display pixels in buffers to display
-    void Display_Base_White() {
-        controller::EPD_13IN3B_Display_Base_White();
-    }
+    // void Display_Base_White() { controller::EPD_13IN3B_Display_Base_White(); } //! Undefined
 
     /// @brief Refresh the display with current buffer
     /// @param mode How to refresh the display
@@ -139,22 +124,16 @@ class EPD_13in3b_DrawTarget : public RedBlack1BitEPD {
 
         switch (static_cast<RefreshMode>(mode)) {
         case RefreshMode::Display:
-        Display();
-        break;
+            Display();
+            break;
 
         case RefreshMode::Base:
-        Display_Base();
-        break;
-
-        case RefreshMode::Base_White:
-        Display_Base_White();
-        break;
-
+            Display_Base();
+            break;
 
         default:
             throw UnsupportedRefreshMode(mode, GetDeviceName());
         }
-        
     }
 
     EPD_13in3b_DrawTarget(bool initializeSPI = true) : RedBlack1BitEPD(GetWidth(), GetHeight(), initializeSPI) {}
