faf3659de264fcf5883feddae5595c84368450a5
Naqeeb Rehman <naq.rehman@gmail.com>
2025-04-27 18:08:31 -0400

    Added text example

---
+++ examples/04 - Text/04_text.cpp
@@ -0,0 +1,69 @@
+/**
+ * 04 | Text
+ *
+ * This example shows how to render text using the TextElement and fonts.
+ */
+
+/**
+ * Import the library headerfile, wsepaperapi.h
+ */
+#include "wsepaperapi.h"
+
+/**
+ * emulate.h contains the code for rendering to an SDL window so that we can visualize our rendered image on screen
+ */
+#include "emulate.h"
+
+/**
+ * Import the display that we will be drawing to. A complete list of displays and their capabilities can be found in
+ * external/waveshare/ParsedCapabilities
+ */
+#include "EPD_2in13b_V4.h"
+
+/**
+ * Import all fonts
+ */
+#include "fonts/AllFonts.h"
+
+using namespace std;
+using namespace epaperapi;
+
+int main() {
+
+    /**
+     * Step 1: Create EpaperEmulator with a physical display as reference
+     */
+
+    // a bit verbose but you can use namespaces to clean it up in your own code...
+    typedef devices::EPD_2in13b_V4::EPD_2in13b_V4_DrawTarget device;
+    EpaperEmulator emulatorTarget = EpaperEmulator::CreatePhysical<device>();
+
+    Renderer renderer(emulatorTarget);
+
+    /**
+     * Step 2: Create lines of text using TextElement
+     */
+
+    elements::TextElement<fonts::DinaBold_10pt> helloWorld("Hello World!");
+    renderer.AddElement(&helloWorld);
+
+    elements::TextElement<fonts::Roboto_12pt> text2("Some fonts are buggy");
+    text2.ypos = 30;
+    renderer.AddElement(&text2);
+
+    elements::TextElement<fonts::Unifont_14pt> text3("Others are good");
+    text3.ypos = 60;
+    renderer.AddElement(&text3);
+
+    // See the scripts/fontmaking folder for more information on creating your own fonts
+
+    /**
+     * Step 3: Refresh the renderer.
+     */
+    renderer.Refresh();
+
+    /**
+     * Step 4: Keep the window open
+     */
+    KeepWindowOpen();
+}
