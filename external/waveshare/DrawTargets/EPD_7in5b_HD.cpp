9240a6d449c33e5da81baf7364b2bd5e15167f33
Naqeeb Rehman <naq.rehman@gmail.com>
2025-04-26 20:31:15 -0400

    Fixed broken file

--- external/waveshare/DrawTargets/EPD_7in5b_HD.cpp
+++ external/waveshare/DrawTargets/EPD_7in5b_HD.cpp
@@ -1,5 +1,5 @@
 extern "C" {
 namespace epaperapi::devices::EPD_7in5b_HD::controller {
-#include "../epaper/e-Paper/EPD_7in5b_HD.c"s
+#include "../epaper/e-Paper/EPD_7in5b_HD.c"
 } // namespace epaperapi::devices::EPD_7in5b_HD::controller
 }
