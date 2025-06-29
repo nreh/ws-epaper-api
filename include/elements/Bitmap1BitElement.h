95671275810ea0c1a4e72318b50dfe72cd04dde0
Naqeeb Rehman <naq.rehman@gmail.com>
2025-06-29 01:33:45 -0400

    Made it so that bitmaps can be changed on Bitmap1BitElement

--- include/elements/Bitmap1BitElement.h
+++ include/elements/Bitmap1BitElement.h
@@ -4,14 +4,12 @@
 namespace epaperapi::elements {
 /// @brief Element that renders a bitmap image
 class Bitmap1BitElement : public AbstractElement {
-  private:
-    const bitmaps::AbstractBitmap& bitmap;
-
   public:
+    bitmaps::AbstractBitmap& bitmap;
     ElementStyle backgroundStyle = ElementStyle::TransparentStyle();
     ElementStyle foregroundStyle = ElementStyle(0);
 
-    Bitmap1BitElement(uint16_t xpos, uint16_t ypos, const bitmaps::AbstractBitmap& bitmap)
+    Bitmap1BitElement(uint16_t xpos, uint16_t ypos, bitmaps::AbstractBitmap& bitmap)
         : bitmap(bitmap), AbstractElement(xpos, ypos) {}
 
     void Draw(AbstractBuffer& target) override {
